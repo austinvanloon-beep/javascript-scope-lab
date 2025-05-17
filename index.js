6b5517196b723e6a5bd208b095391cb4dc74074d
Austin Vanloon <wic404learn@gmail.com>
2025-05-16 18:27:03 -0600

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+// variable data global scope
+let burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// function and block
+function addBurger() {
+  let newBurger = 'Flatburger';
+  burgers.push(newBurger);
+
+  // variable declaration and scope
+  if (true) {
+    let anothernewBurger = 'Maple Bacon Burger';
+    burgers.push(anothernewBurger);
+  }
+}
+
+// function to change featuredDrink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
